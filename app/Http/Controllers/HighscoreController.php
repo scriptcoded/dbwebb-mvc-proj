ffdae5eb1ab48b9de68f08906095b16c6e09c74c
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2021-05-22 18:45:51 +0200

    Highscore filtering

--- app/Http/Controllers/HighscoreController.php
+++ app/Http/Controllers/HighscoreController.php
@@ -9,14 +9,38 @@ use Illuminate\Support\Facades\DB;
 class HighscoreController extends BaseController
 {
     public function index(Request $request)
-    {
+    {        
+        $startDate = null;
+        $endDate = null;
+        $name = null;
+
+        if (!$request->input('reset')) {
+            $startDate = $request->input('startdate');
+            $endDate = $request->input('enddate');
+            $name = $request->input('name');
+        }
+
         $highscores = DB::table('highscores')
             ->select('*', DB::raw('(`player_score`- `computer_score`) AS score'))
-            ->orderBy('score', 'desc')
-            ->get();
+            ->orderBy('score', 'desc');
+
+        if ($startDate) {
+            $highscores->whereDate('created_at', '>=', $startDate);
+        }
+
+        if ($endDate) {
+            $highscores->whereDate('created_at', '<=', $endDate);
+        }
+
+        if ($name) {
+            $highscores->where('name', 'like', '%' . $name . '%');
+        }
 
         return view('highscores', [
-            'highscores' => $highscores,
+            'startDate' => $startDate,
+            'endDate' => $endDate,
+            'name' => $name,
+            'highscores' => $highscores->get(),
             'highlighted' => $request->input('score_id')
         ]);
     }
--- resources/views/highscores.blade.php
+++ resources/views/highscores.blade.php
@@ -1,5 +1,75 @@
 <x-layout>
     <h1 class="title is-1">Highscores</h1>
+    
+
+    <section class="box">
+        <h4 class="title is-4">Filter</h4>
+
+        <form method="GET">
+            <div class="field-body datetime-input mb-3">
+                <div class="field">
+                    <label class="label">From</label>
+                    <div class="control">
+                      <input
+                        class="input"
+                        type="datetime-local"
+                        placeholder="Start date"
+                        name="startdate"
+                        value="{{ $startDate }}"
+                    >
+                    </div>
+                </div>
+            
+                <div class="field">
+                    <label class="label">To</label>
+                    <div class="control">
+                        <input
+                          class="input"
+                          type="datetime-local"
+                          placeholder="End date"
+                          name="enddate"
+                          value="{{ $endDate }}"
+                        >
+                    </div>
+                </div>
+            </div>
+
+            <div class="field">
+                <label class="label">Name</label>
+                <div class="control">
+                    <input
+                      class="input"
+                      type="text"
+                      placeholder="Name"
+                      name="name"
+                      value="{{ $name }}"
+                    >
+                </div>
+            </div>
+    
+            <div class="field is-grouped">
+                <div class="control">
+                  <button
+                    type="submit"
+                    class="button is-link"
+                  >
+                      Apply filter
+                    </button>
+                </div>
+
+                <div class="control">
+                    <button
+                        type="submit"
+                        name="reset"
+                        value="true"
+                        class="button is-danger"
+                    >
+                        Reset filter
+                    </button>
+                </div>
+            </div>
+        </form>
+    </section>
 
     <table class="table">
         <thead>
@@ -45,6 +115,12 @@
                     <td>{{ $highscore->created_at }}</td>
                 </tr>
             @endforeach
+
+            @if (count($highscores) <= 0)
+                <tr>
+                    <td colspan="6">No results</td>
+                </tr>
+            @endif
         </tbody>
     </table>
 </x-layout>
