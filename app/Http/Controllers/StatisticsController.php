02042d9fa71b0c5b7ab9ad9af3fa4a1705f646bd
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2021-05-24 20:33:55 +0200

    Improved statistics

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -10,17 +10,14 @@ class StatisticsController extends BaseController
 {
     public function index(Request $request)
     {
-        // $singleDiceMax = 13;
-        // $singleDiceData = [1, 3, 8, 12, 5, 2];
-
-        $rolls = DB::table('dice_rolls')
+        $singleRolls = DB::table('dice_rolls')
             ->where('dice', '=', 1)
             ->get();
 
         $singleDiceMax = 0;
-        $singleDiceData = [];
+        $singleDiceData = [1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0];
 
-        foreach ($rolls as $roll) {
+        foreach ($singleRolls as $roll) {
             $singleDiceData[$roll->result] = ($singleDiceData[$roll->result] ?? 0) + 1;
 
             if ($singleDiceData[$roll->result] > $singleDiceMax) {
@@ -30,9 +27,28 @@ class StatisticsController extends BaseController
 
         ksort($singleDiceData);
 
+        $dualRolls = DB::table('dice_rolls')
+            ->where('dice', '=', 2)
+            ->get();
+
+        $dualDiceMax = 0;
+        $dualDiceData = [1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0, 8 => 0, 9 => 0, 10 => 0, 11 => 0, 12 => 0];
+
+        foreach ($dualRolls as $roll) {
+            $dualDiceData[$roll->result] = ($dualDiceData[$roll->result] ?? 0) + 1;
+
+            if ($dualDiceData[$roll->result] > $dualDiceMax) {
+                $dualDiceMax = $dualDiceData[$roll->result];
+            }
+        }
+
+        ksort($dualDiceData);
+
         return view('statistics', [
             'singleDiceMax' => $singleDiceMax + 1,
-            'singleDiceData' => $singleDiceData
+            'singleDiceData' => $singleDiceData,
+            'dualDiceMax' => $dualDiceMax + 1,
+            'dualDiceData' => $dualDiceData
         ]);
     }
 }
--- resources/views/statistics.blade.php
+++ resources/views/statistics.blade.php
@@ -1,8 +1,19 @@
 <x-layout>
     <h1 class="title is-1">Statistics</h1>
 
+    <h3 class="title is-3 mt-6">Single dice rolls</h3>
+    <h5 class="subtitle">X: result, Y: frequency</h5>
+
     <x-histogram
         :max="$singleDiceMax"
         :values="$singleDiceData"
+    />
+
+    <h3 class="title is-3 mt-6">Dual dice rolls</h3>
+    <h5 class="subtitle">X: result, Y: frequency</h5>
+
+    <x-histogram
+        :max="$dualDiceMax"
+        :values="$dualDiceData"
     />
 </x-layout>
